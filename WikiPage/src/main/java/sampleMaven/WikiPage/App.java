46be13dc09e20c35d40f463930a981b435e7ec1d
jamisrinivas143 <40839689+jamisrinivas143@users.noreply.github.com>
2018-08-17 01:57:26 +0530

    Add files via upload

---
+++ WikiPage/src/main/java/sampleMaven/WikiPage/App.java
@@ -0,0 +1,13 @@
+package sampleMaven.WikiPage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
